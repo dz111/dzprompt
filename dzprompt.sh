869fa11ae117088e8f4eced87b4cd0690f57bfed
David Zhong <dzhong111@gmail.com>
2024-05-26 19:40:50 +1000

    First commit

---
+++ dzprompt.sh
@@ -0,0 +1,22 @@
+# Add to ~/.bashrc:
+#   source ~/dzprompt/dzprompt.sh
+
+git_branch() {
+  branch="$(git branch --show-current 2> /dev/null)"
+  [[ -n $branch ]] && echo "($branch$(git_dirty)) "
+}
+
+git_dirty() {
+  [[ $(git status --porcelain) ]] && echo "*"
+}
+
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[1;33m'
+BLUE='\e[1;34m'
+MAGENTA='\e[1;35m'
+CYAN='\e[36m'
+WHITE='\e[1;37m'
+ENDCOLOR='\e[0m'
+
+PS1="\n┌${YELLOW}\u@${GREEN}\h ${BLUE}\w ${RED}\$(git_branch)${ENDCOLOR}\n└> "
